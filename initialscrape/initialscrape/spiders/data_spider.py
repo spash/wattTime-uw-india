b0a7c544149f90245e6181e5e6e181a13fcf82e1
Raihamm <raiham@uw.edu>
2022-08-02 16:16:47 -0700

    Trying to scrape first source

---
+++ initialscrape/initialscrape/spiders/data_spider.py
@@ -0,0 +1,27 @@
+import scrapy
+from scrapy import Selector
+
+#When you run this code in your anaconda console or command line
+# you run 'scrapy crawl quotes' where quotes is the name assigned below 
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
+            'https://sldccg.com/trans.php',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+        # requests scrapy url
+    
+    def parse(self, response):
+        tables = [0,0]
+        tables[0] = response.xpath('//*[@id="testdiv"]/table/tbody/tr[2]/td[1]/table').extract()
+        # second table xpath
+        tables[1] = response.xpath('//*[@id="testdiv"]/table/tbody/tr[2]/td[2]/table').extract()
+        for tbl in tables:
+            yield {
+                'Transformer Name': response.xpath('//*[@id="testdiv"]/table/tbody/tr[2]/td[1]/table/tbody/tr[3]/td[1]/text()').extract(),
+                'Load': response.xpath('//*[@id="testdiv"]/table/tbody/tr[2]/td[1]/table/tbody/tr[3]/td[2]/text()').extract(),
+                'Test': 'hi'
+            }
