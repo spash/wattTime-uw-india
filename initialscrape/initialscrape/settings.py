0a3449239fa4b36a36cc5db2e67b1cec699bc9f2
Stephen Sherwood <stephenemails@gmail.com>
2022-08-18 16:02:36 -0700

    Update settings.py

--- initialscrape/initialscrape/settings.py
+++ initialscrape/initialscrape/settings.py
@@ -7,10 +7,6 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-import copy
-from colorlog import ColoredFormatter
-import scrapy.utils.log
-
 
 BOT_NAME = 'initialscrape'
 
@@ -95,29 +91,3 @@ DOWNLOADER_MIDDLEWARES = {
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
-
-
-color_formatter = ColoredFormatter(
-    (
-        '%(log_color)s%(levelname)-5s%(reset)s '
-        '%(yellow)s[%(asctime)s]%(reset)s'
-        '%(white)s %(name)s %(funcName)s %(bold_purple)s:%(lineno)d%(reset)s '
-        '%(log_color)s%(message)s%(reset)s'
-    ),
-    datefmt='%y-%m-%d %H:%M:%S',
-    log_colors={
-        'DEBUG': 'blue',
-        'INFO': 'bold_cyan',
-        'WARNING': 'red',
-        'ERROR': 'bg_bold_red',
-        'CRITICAL': 'red,bg_white',
-    }
-)
-
-_get_handler = copy.copy(scrapy.utils.log._get_handler)
-
-def _get_handler_custom(*args, **kwargs):
-    handler = _get_handler(*args, **kwargs)
-    handler.setFormatter(color_formatter)
-    return handler
-scrapy.utils.log._get_handler = _get_handler_custom
